82a33c09be39783531d7d98c08ccaa5d41eaac41
Xirudly <734428735@qq.com>
2026-03-15 15:42:33 +0800

    Initialize XTurbo Kernel
    Signed-off-by: Xirudly <734428735@qq.com>

--- anykernel.sh
+++ anykernel.sh
@@ -4,32 +4,23 @@
 ### AnyKernel setup
 # global properties
 properties() { '
-kernel.string=ExampleKernel by osm0sis @ xda-developers
+kernel.string=XTurbo Kernel by Xirudly
 do.devicecheck=1
 do.modules=0
 do.systemless=1
 do.cleanup=1
 do.cleanuponabort=0
-device.name1=maguro
-device.name2=toro
-device.name3=toroplus
-device.name4=tuna
-device.name5=
-supported.versions=
-supported.patchlevels=
-supported.vendorpatchlevels=
+device.name1=cepheus
+device.name2=raphael
+device.name3=raphaelin
+device.name4=crux
+supported.versions=11-16
 '; } # end properties
 
 
 ### AnyKernel install
-## boot files attributes
-boot_attributes() {
-set_perm_recursive 0 0 755 644 $RAMDISK/*;
-set_perm_recursive 0 0 750 750 $RAMDISK/init* $RAMDISK/sbin;
-} # end attributes
-
 # boot shell variables
-BLOCK=/dev/block/platform/omap/omap_hsmmc.0/by-name/boot;
+BLOCK=/dev/block/bootdevice/by-name/boot;
 IS_SLOT_DEVICE=0;
 RAMDISK_COMPRESSION=auto;
 PATCH_VBMETA_FLAG=auto;
@@ -38,84 +29,42 @@ PATCH_VBMETA_FLAG=auto;
 . tools/ak3-core.sh;
 
 # boot install
-dump_boot; # use split_boot to skip ramdisk unpack, e.g. for devices with init_boot ramdisk
+split_boot;
 
-# init.rc
-backup_file init.rc;
-replace_string init.rc "cpuctl cpu,timer_slack" "mount cgroup none /dev/cpuctl cpu" "mount cgroup none /dev/cpuctl cpu,timer_slack";
+# patch dtb if using retrofit dynamic partitions
+grep -q "logical" /vendor/etc/fstab.qcom;
+if [ $? -eq 0 ]; then
+    ui_print " " "Retrofit dynamic partitions detected. Patching dtb...";
+    fdtput -r $AKHOME/dtb /firmware/android/vbmeta;
+    fdtput -r $AKHOME/dtb /firmware/android/fstab;
+else
+    # patch dtb if using erofs on /vendor
+    fs_type=$(mount | grep ' /vendor ' | awk '{print $5}');
+    if [ "$fs_type" = "erofs" ]; then
+        ui_print " " "EROFS filesystem type on /vendor detected. Patching dtb...";
+        fdtput -ts $AKHOME/dtb /firmware/android/fstab/vendor type "erofs";
+        fdtput -ts $AKHOME/dtb /firmware/android/fstab/vendor mnt_flags "ro";
+    fi;
+fi;
+cat $AKHOME/Image.gz $AKHOME/dtb > $AKHOME/Image.gz-dtb;
+rm -rf $AKHOME/Image.gz $AKHOME/dtb;
 
-# init.tuna.rc
-backup_file init.tuna.rc;
-insert_line init.tuna.rc "nodiratime barrier=0" after "mount_all /fstab.tuna" "\tmount ext4 /dev/block/platform/omap/omap_hsmmc.0/by-name/userdata /data remount nosuid nodev noatime nodiratime barrier=0";
-append_file init.tuna.rc "bootscript" init.tuna;
-
-# fstab.tuna
-backup_file fstab.tuna;
-patch_fstab fstab.tuna /system ext4 options "noatime,barrier=1" "noatime,nodiratime,barrier=0";
-patch_fstab fstab.tuna /cache ext4 options "barrier=1" "barrier=0,nomblk_io_submit";
-patch_fstab fstab.tuna /data ext4 options "data=ordered" "nomblk_io_submit,data=writeback";
-append_file fstab.tuna "usbdisk" fstab;
-
-write_boot; # use flash_boot to skip ramdisk repack, e.g. for devices with init_boot ramdisk
+flash_boot;
 ## end boot install
 
+android_sdk=""
+mount -o ro,remount /system 2>/dev/null
+mount -o ro /system 2>/dev/null
+if [ -f /system/build.prop ]; then
+  android_sdk=$(sed -n 's/^ro.build.version.sdk=//p' /system/build.prop 2>/dev/null)
+fi
+umount /system 2>/dev/null
 
-## init_boot files attributes
-#init_boot_attributes() {
-#set_perm_recursive 0 0 755 644 $RAMDISK/*;
-#set_perm_recursive 0 0 750 750 $RAMDISK/init* $RAMDISK/sbin;
-#} # end attributes
+android_sdk=${android_sdk:-36}
 
-# init_boot shell variables
-#BLOCK=init_boot;
-#IS_SLOT_DEVICE=1;
-#RAMDISK_COMPRESSION=auto;
-#PATCH_VBMETA_FLAG=auto;
+if [ "$android_sdk" -le 35 ]; then
+  ui_print " " "Detected that the current Android version is Android15 or below, flashing the old dtbo.";
+  cp -f "$AKHOME/dtbo_old.img" "$AKHOME/dtbo.img"
+fi
 
-# reset for init_boot patching
-#reset_ak;
-
-# init_boot install
-#dump_boot; # unpack ramdisk since it is the new first stage init ramdisk where overlay.d must go
-
-#write_boot;
-## end init_boot install
-
-
-## vendor_kernel_boot shell variables
-#BLOCK=vendor_kernel_boot;
-#IS_SLOT_DEVICE=1;
-#RAMDISK_COMPRESSION=auto;
-#PATCH_VBMETA_FLAG=auto;
-
-# reset for vendor_kernel_boot patching
-#reset_ak;
-
-# vendor_kernel_boot install
-#split_boot; # skip unpack/repack ramdisk, e.g. for dtb on devices with hdr v4 and vendor_kernel_boot
-
-#flash_boot;
-## end vendor_kernel_boot install
-
-
-## vendor_boot files attributes
-#vendor_boot_attributes() {
-#set_perm_recursive 0 0 755 644 $RAMDISK/*;
-#set_perm_recursive 0 0 750 750 $RAMDISK/init* $RAMDISK/sbin;
-#} # end attributes
-
-# vendor_boot shell variables
-#BLOCK=vendor_boot;
-#IS_SLOT_DEVICE=1;
-#RAMDISK_COMPRESSION=auto;
-#PATCH_VBMETA_FLAG=auto;
-
-# reset for vendor_boot patching
-#reset_ak;
-
-# vendor_boot install
-#dump_boot; # use split_boot to skip ramdisk unpack, e.g. for dtb on devices with hdr v4 but no vendor_kernel_boot
-
-#write_boot; # use flash_boot to skip ramdisk repack, e.g. for dtb on devices with hdr v4 but no vendor_kernel_boot
-## end vendor_boot install
-
+flash_dtbo;
